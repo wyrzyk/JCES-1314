db562feb7d0bfda430725d3692bfb271f951a9b7
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-03-11 10:12:36 +0100

    JCES-1248: Try cookie login on AWS VUs

--- src/test/kotlin/JiraPerformanceComparisonIT.kt
+++ src/test/kotlin/JiraPerformanceComparisonIT.kt
@@ -11,8 +11,6 @@ import com.atlassian.performance.tools.workspace.api.RootWorkspace
 import com.atlassian.performance.tools.workspace.api.TestWorkspace
 import jces1209.BenchmarkQuality
 import jces1209.EagerSlowAndMeaningful
-import jces1209.QuickAndDirty
-import jces1209.SlowAndMeaningful
 import jces1209.log.LogConfigurationFactory
 import jces1209.vu.JiraCloudScenario
 import org.apache.logging.log4j.core.config.ConfigurationFactory
@@ -26,7 +24,7 @@ import java.util.concurrent.Executors
 class JiraPerformanceComparisonIT {
 
     private val workspace = RootWorkspace(Paths.get("build")).currentTask
-    private val benchmarkQuality: BenchmarkQuality = QuickAndDirty()
+    private val benchmarkQuality: BenchmarkQuality = EagerSlowAndMeaningful()
 
     init {
         ConfigurationFactory.setConfigurationFactory(LogConfigurationFactory(workspace))
