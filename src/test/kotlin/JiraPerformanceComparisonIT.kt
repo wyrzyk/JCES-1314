61e6ade13f2191edf8db95d48fcc8ac1173c32fc
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2020-04-22 15:06:14 +0200

    JCES-1314: 5h/5h

--- src/test/kotlin/JiraPerformanceComparisonIT.kt
+++ src/test/kotlin/JiraPerformanceComparisonIT.kt
@@ -29,8 +29,8 @@ class JiraPerformanceComparisonIT {
     private val workspace = RootWorkspace(Paths.get("build")).currentTask
     private val quality: BenchmarkQuality = SlowAndMeaningful
         .Builder()
-        .ramp(Duration.ofMinutes(150))
-        .flat(Duration.ofMinutes(150))
+        .ramp(Duration.ofHours(5))
+        .flat(Duration.ofMinutes(5))
         .build()
 
     init {
